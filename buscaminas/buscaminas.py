ad4df19e59bbed5104af6316635231f31bf87c10
root <root@LAPTOP-E2VA86HO.localdomain>
2023-12-18 13:37:57 -0300

    buscaminas terminado

--- buscaminas/buscaminas.py
+++ buscaminas/buscaminas.py
@@ -25,7 +25,7 @@ class Buscaminas(Subject):
         self.tablero = [[' ' for _ in range(columnas)] for _ in range(filas)]
         self.minas_tablero = [[0 for _ in range(columnas)] for _ in range(filas)]
         self.juego_terminado = False
-
+    #colocar minas aleatorias en el tablero
     def colocar_minas(self):
         minas_colocadas = 0
         while minas_colocadas < self.minas:
@@ -34,7 +34,7 @@ class Buscaminas(Subject):
             if self.minas_tablero[fila][columna] != -1:
                 self.minas_tablero[fila][columna] = -1
                 minas_colocadas += 1
-
+    #contador de minas en un area circundante
     def contar_minas_alrededor(self, fila, columna):
         if self.minas_tablero[fila][columna] == -1:
             return -1
@@ -52,7 +52,7 @@ class Buscaminas(Subject):
             for columna in range(self.columnas):
                 if self.minas_tablero[fila][columna] != -1:
                     self.minas_tablero[fila][columna] = self.contar_minas_alrededor(fila, columna)
-
+    #muestra el tablero
     def mostrar_tablero(self, mostrar_minas=False):
         for fila in range(self.filas):
             for columna in range(self.columnas):
@@ -64,7 +64,7 @@ class Buscaminas(Subject):
                     print(self.tablero[fila][columna], end=" ")
             print()
         print()
-
+    #lo que pasa al hacer click izquierod
     def descubrir_casilla(self, fila, columna):
         if self.tablero[fila][columna] == 'X':
             return
@@ -80,6 +80,7 @@ class Buscaminas(Subject):
                     for j in range(max(0, columna - 1), min(self.columnas, columna + 2)):
                         if self.tablero[i][j] == ' ':
                             self.descubrir_casilla(i, j)
+    #marcador intero para la condicion de victoria
     def contar_marcadorInterno(self):
         contador = 0
         for fila in range(self.filas):
@@ -87,6 +88,7 @@ class Buscaminas(Subject):
                 if self.tablero[fila][columna] == 'X' and self.minas_tablero[fila][columna] == -1:
                     contador += 1
         return contador
+    #contador de marcadores que se muestran en pantalla
     def contar_marcadores(self):
         contador = 0
         for fila in range(self.filas):
@@ -110,19 +112,14 @@ class BuscaminasGUI:
         self.font = pygame.font.Font(None, 24)
         self.marcadores = 0
         self.marcadorInterno=0
-        self.colors = {
-            -1: (255, 0, 0),   # Mina (rojo)
-            0: (255, 255, 0),  # Cero (amarillo)
-            1: (0, 0, 255),    # Número 1 (azul)
-            # Añade más colores para los números restantes según sea necesario
-        }
+        
 
     def draw_grid(self):
         for y in range(0, self.height, self.tile_size):
             for x in range(0, self.width, self.tile_size):
                 rect = pygame.Rect(x, y, self.tile_size, self.tile_size)
                 pygame.draw.rect(self.screen, (200, 200, 200), rect, 1)
-
+    #dibujar los numeros, la mina es -1, indica el numero de minas en su vecindad o blanco si no hay nada en su vecindad
     def draw_numbers(self):
         for i in range(self.filas):
             for j in range(self.columnas):
@@ -143,13 +140,13 @@ class BuscaminasGUI:
                         text_rect = text.get_rect(center=(j * self.tile_size + self.tile_size // 2,
                                                           i * self.tile_size + self.tile_size // 2))
                         self.screen.blit(text, text_rect)
-
+    #poner marcadores
     def draw_marcadores(self):
         marcador_text = self.font.render(f"Marcadores: {self.juego.contar_marcadores()}/{self.minas}", True, (0, 0, 0))
         marcador_rect = marcador_text.get_rect()
         marcador_rect.topleft = (10, self.height + 10)  # Posición ajustada para los marcadores
         self.screen.blit(marcador_text, marcador_rect)
-
+    #actualizar pantalla
     def update(self):
         self.width = self.columnas * self.tile_size
         self.height = self.filas * self.tile_size
@@ -166,23 +163,23 @@ class BuscaminasGUI:
         self.draw_marcadores()
 
         pygame.display.flip()
-
+    #implementa las acciones que se realizan en el juego
     def run(self):
         while not self.juego.juego_terminado:
             self.update()
-            for event in pygame.event.get():
+            for event in pygame.event.get():#cerrar el juego
                 if event.type == QUIT:
                     pygame.quit()
                     return
-                if event.type == MOUSEBUTTONDOWN:
+                if event.type == MOUSEBUTTONDOWN:#presionar un boton
                     x, y = pygame.mouse.get_pos()
                     fila = y // self.tile_size
                     columna = x // self.tile_size
 
-                    if event.button == 1:  # Click izquierdo
+                    if event.button == 1:  # Click izquierdo abrir casilla
                         self.juego.descubrir_casilla(fila, columna)
                         self.update()
-                    elif event.button == 3:  # Click derecho
+                    elif event.button == 3:  # Click derecho marcar casilla
                         if self.juego.tablero[fila][columna] == ' ':
                             self.juego.tablero[fila][columna] = 'X'
                             self.marcadores += 1
@@ -191,7 +188,7 @@ class BuscaminasGUI:
                             self.juego.tablero[fila][columna] = ' '
                             self.marcadores -= 1
                             self.update()
-
+                    #condicion de victoria, si el contador de marcadores es igual a de minas y a su vez todas las minas estan marcadas, se gana el juego
                     if self.juego.contar_marcadorInterno() == self.minas and self.marcadorInterno == self.minas:
                         print("¡Has ganado!")
                         self.juego.juego_terminado = True
@@ -202,15 +199,16 @@ class BuscaminasGUI:
                         return
 
             self.clock.tick(3)
+# Pantalla en la que se selecciona la dificultad        
 class PantallaInicial:
     def __init__(self):
         pygame.init()
-        self.width = 300
+        self.width = 300# Tamaño de la pantalla
         self.height = 200
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font(None, 36)
-        self.opciones = ["Fácil", "Intermedio", "Difícil"]
+        self.opciones = ["Fácil", "Intermedio", "Difícil"]#Niveles de dificultad
         self.selected_option = None
         self.finished = False
 
